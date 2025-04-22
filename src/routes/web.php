55abd3afd046651db23aa5cfc092eba8e8d02d8a
AlexLiCa <alexlin.carre@gmail.com>
2025-04-21 20:19:13 -0600

    -Cambios para que funcionen las cosas de huber , revisar en un futuro

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request; // Import Request
+use App\Models\DeviceLog; // Import DeviceLog model
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) { // Inject Request
+    // Log the current request
+    $log = new DeviceLog();
+    $log->ip_address = $request->ip();
+    $log->user_agent = $request->userAgent();
+    $log->save();
+
+    // Fetch all logs, newest first
+    $deviceLogs = DeviceLog::latest()->get();
+
+    // Pass logs to the view
+    return view('welcome', ['deviceLogs' => $deviceLogs]);
 });
